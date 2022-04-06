5993baae61afd3749921d0c21c20e479a48a83b2
songjiujiu <2032824853@qq.com>
2022-04-06 19:06:50 +0800

    vue3

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <diV id="counter">
+     Couter:{{counter}}
+  </diV>
+</template>
+
+<script >
+export default {
+    data() {
+        return {
+            counter: 0
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
